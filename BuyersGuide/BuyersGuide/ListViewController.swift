cd6c18428ce79ad9bd3c2e59a7be196d7f77dd3c
Kanjanaporn <Bamspint@gmail.com>
2019-08-27 16:41:30 +0700

    -model class-

---
+++ BuyersGuide/BuyersGuide/ListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BuyersGuide
+//
+//  Created by Kanjanaporn on 27/8/2562 BE.
+//  Copyright © 2562 SCBiOS. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BuyersGuide/BuyersGuide/Model.swift
@@ -0,0 +1,30 @@
+//
+//  Model.swift
+//  BuyersGuide
+//
+//  Created by Kanjanaporn on 27/8/2562 BE.
+//  Copyright © 2562 SCBiOS. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+struct Phone {
+    let id : Int
+    let name: String
+    let price: Float
+    let thumbImageURL: String
+    let rating: Float
+    let description: String
+}
+
+extension Phone {
+    init(json: JSON) {
+        id = json["id"].intValue
+        name = json["name"].stringValue
+        price = json["price"].floatValue
+        rating = json["rating"].floatValue
+        description = json["description"].stringValue
+        thumbImageURL = json["thumbImageURL"].stringValue
+    }
+}
